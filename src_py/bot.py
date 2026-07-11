26349dcf157227eed90fcb3317d030594b6c5c6d
Yassperx <elmessariyassine15@gmail.com>
2026-07-11 21:55:37 +0100

    Making bot.py parse args

--- src_py/bot.py
+++ src_py/bot.py
@@ -1,19 +1,25 @@
 import os
 import discord
 import bpe_core
+import sys
 
 class MyClient(discord.Client):
     bpe: bpe_core.Bpe
+    tok_file : str
 
-    def __init__(self, **kwargs):
+    def __init__(self, tok_file: str, **kwargs):
         super().__init__(**kwargs)
         self.bpe = bpe_core.Bpe("")
+        self.tok_file = tok_file
         
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        self.bpe.load_from_file("outputs/big_data.txt")
+        self.bpe.load_from_file(self.tok_file)
 
     async def on_message(self, message):
+        if message.author.bot:
+            return
+
         if not message.content:
             return
 
@@ -37,10 +43,22 @@ class MyClient(discord.Client):
             return
 
         await message.reply(ret)
+    
+def main(args: list[str]):
+    intents = discord.Intents.default()
+    intents.message_content = True
+    print(args)
+    if len(args) <= 1:
+        bpe_core.eprint("Usage: %s <file>" % args[0])
+        return
+    tok_file = args[1]
+    client = MyClient(tok_file=tok_file, intents=intents)
+    token = os.environ.get('API_KEY')
+    if token is None:
+        bpe_core.eprint("Source env.sh")
+        return
+    client.run(token)
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-token = os.environ.get('API_KEY')
-client.run(token)
+if __name__ == "__main__":
+    args = sys.argv
+    main(args)
